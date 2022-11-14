6ace39f9e87dbd34552e283bd5fe33a228d04211
Mateus-msj3 <mateussouzaads@outlook.com>
2022-11-14 15:43:53 -0300

    feat: componente para o cadastro de pessoas

--- src/app/commons/module/commons/commons.module.ts
+++ src/app/commons/module/commons/commons.module.ts
@@ -4,21 +4,32 @@ import {HeaderComponent} from "../../componente/header/header.component";
 import {TabMenuModule} from "primeng/tabmenu";
 import {CardModule} from "primeng/card";
 import {FooterComponent} from "../../componente/footer/footer.component";
+import {PessoaComponent} from "../../../componente/pessoa/pessoa.component";
+import {InputTextModule} from "primeng/inputtext";
+import {InputMaskModule} from "primeng/inputmask";
+import {CalendarModule} from "primeng/calendar";
+import {FieldsetModule} from "primeng/fieldset";
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
+    PessoaComponent,
   ],
   imports: [
     CommonModule,
     TabMenuModule,
     CardModule,
+    InputTextModule,
+    InputMaskModule,
+    CalendarModule,
+    FieldsetModule
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
+    PessoaComponent,
   ],
   providers: [
 
