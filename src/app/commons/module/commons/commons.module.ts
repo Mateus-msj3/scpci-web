0fa905fb1285c7754c5cbcbf91076cdd3ea3848e
Mateus-msj3 <mateussouzaads@outlook.com>
2022-11-13 23:49:07 -0300

    feat: adicionado o footer e o tema do material design

--- src/app/commons/module/commons/commons.module.ts
+++ src/app/commons/module/commons/commons.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {HeaderComponent} from "../../componente/header/header.component";
 import {TabMenuModule} from "primeng/tabmenu";
 import {CardModule} from "primeng/card";
+import {FooterComponent} from "../../componente/footer/footer.component";
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
@@ -16,6 +18,7 @@ import {CardModule} from "primeng/card";
   ],
   exports: [
     HeaderComponent,
+    FooterComponent,
   ],
   providers: [
 
